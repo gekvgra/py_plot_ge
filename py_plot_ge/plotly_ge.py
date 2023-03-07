23236b4ef1e144566685258cfc22800562a32048
yannick.schwarz <yannick.schwarz@kvg.org>
2023-03-07 17:56:56 +0100

    update

--- py_plot_ge/plotly_ge.py
+++ py_plot_ge/plotly_ge.py
@@ -1,4 +1,4 @@
-"""Main module."""
+"""This module provides functions for the layout of plots or similar in line with the corprorate design of GE KVG."""
 
 
 def plotly_gelayout(
@@ -11,6 +11,21 @@ def plotly_gelayout(
     hovermode="x",
     font_size=12
 ):
+    """
+    Update a plotly object with the layout defaults of GE KVG.
+
+    Args:
+        plotly_object: A plotly object.
+        plot_title,xaxis_title,yaxis_title,yaxis2_title,legend_title Character vectors with the titles.
+        legend_h_adjust: Numeric value which can be positive and negative. Default is -0.1.
+        hovermode: Character vector with default "x". Other possible values are "y", "closest", FALSE, "x unified" and "y unified".
+        legend_title_space: Character vector with white space to align legend title.
+        yaxis2: Logical if a secondary y-axis is desired. Default is FALSE.
+        yaxis2_args: Further arguments which are passed on to \code{layout}.
+        font_size: Integer with the font size.
+    Returns:
+        A plotly object with the updated layout.
+    """
     plotly_object.update_layout(
         title=plot_title,
         xaxis=dict(
